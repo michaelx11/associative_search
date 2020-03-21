98a0d46c661252d40063afdf86173912a6433c32
Ramen Deer <ramendeer@gmail.com>
2020-03-21 02:51:29 -0700

    can actually do search now

--- searcher/src/main.rs
+++ searcher/src/main.rs
@@ -20,23 +20,17 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-fn find_associations(search_set: &[String], preloaded_lines: &[Vec<String>]) -> HashMap<String, HashMap<String, String>> {
-    // map[item]-> map[article]->title
+fn find_associations(search_set: &[&str], norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> HashMap<String, HashMap<String, String>> {
     let mut association_dict: HashMap<String, HashMap<String, String>> = HashMap::new();
-    for article_vec in preloaded_lines {
-        let title = article_vec[0].to_string();
-        // Iterate through items in search set
-        for item in search_set.iter() {
-            let item_key = item.to_string();
-            // SELECTION CRITERIA - does title match item?
-            if title.contains(&item_key) {
-                let entry = association_dict.entry(item_key).or_insert_with(HashMap::new);
-                // If so, go ahead and add articles->title
-                for article in article_vec[1..article_vec.len()].iter() {
-                    let article_string = article.as_str();
-                    entry.insert(article_string.to_string(), title.to_string());
-                }
-            }
+    for term in search_set {
+        let entry = association_dict.entry(term.to_string()).or_insert_with(HashMap::new);
+        let norm_results = indexer::search_fst_index(&term, &norm_index, 1);
+        let table_results = indexer::search_fst_index(&term, &table_index, 1);
+        for (article, title) in norm_results {
+            entry.insert(article.to_string(), title.to_string());
+        }
+        for (article, title) in table_results {
+            entry.insert(article.to_string(), title.to_string());
         }
     }
     return association_dict;
@@ -66,6 +60,31 @@ fn subfind_associations(associations: &HashMap<String, HashMap<String, String>>,
     return association_dict;
 }
 
+fn process_query(query: &str, norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> String {
+    // Get query set, split by ","
+    let search_terms: Vec<&str> = query.split(",").collect();
+    let mut association_dict = find_associations(&search_terms[..], norm_index, table_index);
+    // Finally, we check if we got any good associations
+    let mut association_count_dict: HashMap<String, usize> = HashMap::new();
+    for item in search_terms.iter() {
+        let item_key = item.to_string();
+        for (key, value) in association_dict.entry(item_key).or_insert_with(HashMap::new) {
+            let key_string = key.to_string();
+            association_count_dict.entry(key_string).and_modify(|e| {*e += 1}).or_insert(1);
+        }
+    }
+    for (assoc, count) in association_count_dict {
+        if count >= search_terms.len() {
+            for item in search_terms.iter() {
+                let item_string = item.to_string();
+                let assoc_string = assoc.to_string();
+                println!("{}: {}", assoc, association_dict[&item_string].get(&assoc).unwrap_or(&"[NONE]".to_string()));
+            }
+        }
+    }
+    return "".to_string();
+}
+
 fn main() {
     // first arg: filename, remaining args go into search set
     let args: Vec<String> = env::args().collect();
@@ -78,8 +97,11 @@ fn main() {
     // Search set is a list of search items
     let search_set = &args[3..args.len()];
     eprintln!("filename: {:?}, threshold: {:?}, search set: {:?}", filename, threshold, search_set);
+    let table_index_filename = "big_table_index.txt";
+    let norm_index_filename = "big_norm_index.txt";
     let now = Instant::now();
-    let index = indexer::generate_fst_index(filename, 1).unwrap();
+    let table_index = indexer::generate_fst_index(table_index_filename, 1).unwrap();
+    let norm_index = indexer::generate_fst_index(norm_index_filename, 1).unwrap();
     println!("finished indexing in {}s", now.elapsed().as_secs());
     while true {
         let mut line = String::new();
@@ -87,7 +109,7 @@ fn main() {
         let stdin = io::stdin();
         stdin.lock().read_line(&mut line).unwrap();
         println!("Searching: {}", &line);
-        let results = indexer::search_fst_index(&line, &index, 1);
+        let results = process_query(&line, &norm_index, &table_index);
         println!("Results: {:?}", results);
     }
 //    // Need a mapping from items to article

93d5784d10c5b5ae48afb9ef2e8852d6a162945e
Ramen Deer <ramendeer@gmail.com>
2020-03-22 00:12:46 -0700

    adding structure to queries

--- searcher/src/main.rs
+++ searcher/src/main.rs
@@ -13,6 +13,17 @@ use serde_json::Value;
 
 use searcher::indexer;
 
+enum QueryStage {
+    WikiAll,
+    WikiArticleRefs,
+    Synonym
+}
+
+struct Query {
+    query_terms: Vec<String>,
+    stages: Vec<QueryStage>
+}
+
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -20,7 +31,7 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-fn find_associations(search_set: &[&str], norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> HashMap<String, HashMap<String, String>> {
+fn find_associations(search_set: &[String], norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> HashMap<String, HashMap<String, String>> {
     let mut association_dict: HashMap<String, HashMap<String, String>> = HashMap::new();
     for term in search_set {
         let entry = association_dict.entry(term.to_string()).or_insert_with(HashMap::new);
@@ -60,13 +71,25 @@ fn subfind_associations(associations: &HashMap<String, HashMap<String, String>>,
     return association_dict;
 }
 
-fn process_query(query: &str, norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> String {
-    // Get query set, split by ","
-    let search_terms: Vec<&str> = query.split(",").collect();
-    let mut association_dict = find_associations(&search_terms[..], norm_index, table_index);
+fn process_query(query: &Query, norm_index: &indexer::FstIndex, table_index: &indexer::FstIndex) -> String {
+    let mut association_dict: HashMap<String, HashMap<String, String>> = HashMap::new();
+    for stage in query.stages.iter() {
+        match stage {
+            QueryStage::WikiAll => {
+                association_dict.extend(find_associations(&query.query_terms[..], norm_index, table_index));
+            },
+            QueryStage::WikiArticleRefs => {
+                // TODO: fix this hack
+                association_dict.extend(find_associations(&query.query_terms[..], norm_index, norm_index));
+            },
+            QueryStage::Synonym => {
+                // TODO: implement
+            },
+        }
+    }
     // Finally, we check if we got any good associations
     let mut association_count_dict: HashMap<String, usize> = HashMap::new();
-    for item in search_terms.iter() {
+    for item in query.query_terms.iter() {
         let item_key = item.to_string();
         for (key, value) in association_dict.entry(item_key).or_insert_with(HashMap::new) {
             let key_string = key.to_string();
@@ -74,8 +97,8 @@ fn process_query(query: &str, norm_index: &indexer::FstIndex, table_index: &inde
         }
     }
     for (assoc, count) in association_count_dict {
-        if count >= search_terms.len() {
-            for item in search_terms.iter() {
+        if count >= query.query_terms.len() {
+            for item in query.query_terms.iter() {
                 let item_string = item.to_string();
                 let assoc_string = assoc.to_string();
                 println!("{}: {}", assoc, association_dict[&item_string].get(&assoc).unwrap_or(&"[NONE]".to_string()));
@@ -83,6 +106,17 @@ fn process_query(query: &str, norm_index: &indexer::FstIndex, table_index: &inde
         }
     }
     return "".to_string();
+}
+
+fn parse_interactive_query(query: &str) -> Query {
+    // Get query set, split by ","
+    let mut query_terms: Vec<String> = Vec::new();
+    for term in query.split(",") {
+        query_terms.push(term.to_string());
+    }
+    let mut stages: Vec<QueryStage> = Vec::new();
+    stages.push(QueryStage::WikiAll);
+    return Query{query_terms, stages};
 }
 
 fn main() {
@@ -109,7 +143,8 @@ fn main() {
         let stdin = io::stdin();
         stdin.lock().read_line(&mut line).unwrap();
         println!("Searching: {}", &line);
-        let results = process_query(&line, &norm_index, &table_index);
+        let query = parse_interactive_query(&line);
+        let results = process_query(&query, &norm_index, &table_index);
         println!("Results: {:?}", results);
     }
 //    // Need a mapping from items to article
